650898f1b34844e858fd32dfc8790f2bf9205a7e
mihanik2828 <mihanik2828@yandex.ru>
2022-11-01 18:16:36 +0300

    First load
    Early version

---
+++ dispatcher/src/main/java/ru/ionov/controller/UpdateController.java
@@ -0,0 +1,84 @@
+package ru.ionov.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.ionov.service.UpdateProducer;
+import ru.ionov.utils.MessageUtils;
+
+import static ru.ionov.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private final MessageUtils messageUtils;
+    private final UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Received object is null");
+            return;
+        }
+        if(update.getMessage() != null) {
+            distributeMessageByType(update);
+        }
+        else{
+            log.error("Unsupported message type is received" + update);
+        }
+
+    }
+
+    private void distributeMessageByType(Update update) {
+        var message = update.getMessage();
+        if(message.getText() != null)
+            processTextMessage(update);
+        else if (message.getDocument() != null)
+            processDocMessage(update);
+        else if (message.getPhoto() != null)
+            processPhotoMessage(update);
+        else
+            setUnsupportedMessageTypeView(update);
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Неподдерживаемый тип сообщения");
+        setView(sendMessage);
+    }
+
+    private void setFileIsReceiverView(Update update) {
+        SendMessage sendMessage = messageUtils.generateSendMessageWithText(update,"Файл получен! Обрабатывается...");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE , update);
+        setFileIsReceiverView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE , update);
+        setFileIsReceiverView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE , update);
+    }
+
+
+}
---
+++ dispatcher/src/main/java/ru/ionov/service/impl/UpdateProducerImpl.java
@@ -0,0 +1,17 @@
+package ru.ionov.service.impl;
+
+import lombok.extern.log4j.Log4j;
+import org.jvnet.hk2.annotations.Service;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.ionov.service.UpdateProducer;
+
+@Service
+@Log4j
+@Component
+public class UpdateProducerImpl implements UpdateProducer {
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+    }
+}
